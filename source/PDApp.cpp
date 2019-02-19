0e9a8cc5aba6c5bc764d253026c964905ed4764e
at669 <31416491+at669@users.noreply.github.com>
2019-02-18 21:20:03 -0500

    Initial commit

---
+++ source/PDApp.cpp
@@ -0,0 +1,153 @@
+//
+//  PDApp.cpp
+//  Poly Demo
+//
+//  This is the root class for your game.  The file main.cpp accesses this class
+//  to run the application.  While you could put most of your game logic in
+//  this class, we prefer to break the game up into player modes and have a
+//  class for each mode.
+//
+//  Author: Walker White and Anthony Perello
+//  Version: 1/29/17
+//
+#include "PDApp.h"
+
+using namespace cugl;
+
+
+#pragma mark -
+#pragma mark Application State
+
+/**
+ * The method called after OpenGL is initialized, but before running the application.
+ *
+ * This is the method in which all user-defined program intialization should
+ * take place.  You should not create a new init() method.
+ *
+ * When overriding this method, you should call the parent method as the
+ * very last line.  This ensures that the state will transition to FOREGROUND,
+ * causing the application to run.
+ */
+void PolyApp::onStartup() {
+    _assets = AssetManager::alloc();
+    _batch  = SpriteBatch::alloc();
+    
+    // Start-up basic input
+#ifdef CU_TOUCH_SCREEN
+    Input::activate<Touchscreen>();
+    Input::activate<PanInput>();
+#else
+    Input::activate<Mouse>();
+#endif
+    _assets->attach<Font>(FontLoader::alloc()->getHook());
+    _assets->attach<Texture>(TextureLoader::alloc()->getHook());
+    _assets->attach<Node>(SceneLoader::alloc()->getHook());
+
+    // Create a "loading" screen
+    _loaded = false;
+    _loading.init(_assets);
+    
+    // Que up the other assets
+    _assets->loadDirectoryAsync("json/assets.json",nullptr);
+    
+    Application::onStartup(); // YOU MUST END with call to parent
+}
+
+/**
+ * The method called when the application is ready to quit.
+ *
+ * This is the method to dispose of all resources allocated by this
+ * application.  As a rule of thumb, everything created in onStartup()
+ * should be deleted here.
+ *
+ * When overriding this method, you should call the parent method as the
+ * very last line.  This ensures that the state will transition to NONE,
+ * causing the application to be deleted.
+ */
+void PolyApp::onShutdown() {
+    _loading.dispose();
+    _gameplay.dispose();
+    _assets = nullptr;
+    _batch = nullptr;
+    
+    // Shutdown input
+#ifdef CU_TOUCH_SCREEN
+    Input::deactivate<Touchscreen>();
+#else
+    Input::deactivate<Mouse>();
+#endif
+    
+    Application::onShutdown();  // YOU MUST END with call to parent
+}
+
+/**
+ * The method called when the application is suspended and put in the background.
+ *
+ * When this method is called, you should store any state that you do not
+ * want to be lost.  There is no guarantee that an application will return
+ * from the background; it may be terminated instead.
+ *
+ * If you are using audio, it is critical that you pause it on suspension.
+ * Otherwise, the audio thread may persist while the application is in
+ * the background.
+ */
+void PolyApp::onSuspend() {
+}
+
+/**
+ * The method called when the application resumes and put in the foreground.
+ *
+ * If you saved any state before going into the background, now is the time
+ * to restore it. This guarantees that the application looks the same as
+ * when it was suspended.
+ *
+ * If you are using audio, you should use this method to resume any audio
+ * paused before app suspension.
+ */
+void PolyApp::onResume() {
+}
+
+
+#pragma mark -
+#pragma mark Application Loop
+
+/**
+ * The method called to update the application data.
+ *
+ * This is your core loop and should be replaced with your custom implementation.
+ * This method should contain any code that is not an OpenGL call.
+ *
+ * When overriding this method, you do not need to call the parent method
+ * at all. The default implmentation does nothing.
+ *
+ * @param timestep  The amount of time (in seconds) since the last frame
+ */
+void PolyApp::update(float timestep) {
+    if (!_loaded && _loading.isActive()) {
+        _loading.update(0.01f);
+    } else if (!_loaded) {
+        _loading.dispose(); // Disables the input listeners in this mode
+        _gameplay.init(_assets);
+        _loaded = true;
+    } else {
+        _gameplay.update(timestep);
+    }
+}
+
+/**
+ * The method called to draw the application to the screen.
+ *
+ * This is your core loop and should be replaced with your custom implementation.
+ * This method should OpenGL and related drawing calls.
+ *
+ * When overriding this method, you do not need to call the parent method
+ * at all. The default implmentation does nothing.
+ */
+void PolyApp::draw() {
+    if (!_loaded) {
+        _loading.render(_batch);
+    } else {
+        _gameplay.render(_batch);
+    }
+}
+
---
+++ source/PDInput.cpp
@@ -0,0 +1,235 @@
+//
+//  PDInput.cpp
+//  Poly Demo
+//
+//  This input controller is primarily designed for mouse control.  On mobile
+//  you will notice that we use a two finger pan gesture to emulate this.
+//  While the use of the mouse necessitates additional state for tracking movement,
+//  our PanInput class does not use such state, and can set _pandelta directly.
+//
+//  This file is based on the CS 3152 PhysicsDemo Lab by Don Holden, 2007
+//
+//  Author: Walker White and Anthony Perello
+//  Version: 1/29/17
+//
+#include "PDInput.h"
+
+using namespace cugl;
+
+#pragma mark Input Constants
+
+/** The key to use for reseting the game */
+#define RESET_KEY KeyCode::R
+/** The key for exitting the game */
+#define EXIT_KEY  KeyCode::ESCAPE
+
+/** The key for the event handlers */
+#define LISTENER_KEY        1
+
+/** The number of fingers to be used for a pan gesture */
+#define NUM_PAN_FINGERS 2
+
+
+#pragma mark -
+#pragma mark Input Controller
+/**
+* Creates a new input controller.
+*
+* This constructor does NOT do any initialzation.  It simply allocates the
+* object. This makes it safe to use this class without a pointer.
+*/
+PolyInput::PolyInput() :
+	_active(false),
+	_resetPressed(false),
+	_exitPressed(false),
+	_keyReset(false),
+	_keyExit(false),
+	_mousepan(false) {
+}
+
+/**
+* Deactivates this input controller, releasing all listeners.
+*
+* This method will not dispose of the input controller. It can be reused
+* once it is reinitialized.
+*/
+void PolyInput::dispose() {
+	if (_active) {
+#ifndef CU_TOUCH_SCREEN
+		Input::deactivate<Keyboard>();
+		Mouse* mouse = Input::get<Mouse>();
+		mouse->removePressListener(LISTENER_KEY);
+		mouse->removeReleaseListener(LISTENER_KEY);
+		mouse->removeDragListener(LISTENER_KEY);
+#else
+		PanInput* pan = Input::get<PanInput>();
+		pan->removeMotionListener(LISTENER_KEY);
+		pan->removeEndListener(LISTENER_KEY);
+#endif
+		_active = false;
+	}
+}
+
+/**
+* Initializes the input control for the given drawing scale.
+*
+* This method works like a proper constructor, initializing the input
+* controller and allocating memory.  However, it still does not activate
+* the listeners.  You must call start() do that.
+*
+* @return true if the controller was initialized successfully
+*/
+bool PolyInput::init() {
+	bool success = true;
+
+#ifndef CU_TOUCH_SCREEN
+	success = Input::activate<Keyboard>();
+	success = success && Input::activate<Mouse>();
+	Mouse* mouse = Input::get<Mouse>();
+	// Set pointer awareness to always so listening for drags registers
+	// See addDragListener for an explanation
+	mouse->setPointerAwareness(cugl::Mouse::PointerAwareness::ALWAYS);
+	mouse->addPressListener(LISTENER_KEY, [=](const cugl::MouseEvent& event, Uint8 clicks, bool focus) {
+		this->mouseDownCB(event, clicks, focus);
+	});
+	mouse->addReleaseListener(LISTENER_KEY, [=](const cugl::MouseEvent& event, Uint8 clicks, bool focus) {
+		this->mouseUpCB(event, clicks, focus);
+	});
+	mouse->addMotionListener(LISTENER_KEY, [=](const cugl::MouseEvent& event, const cugl::Vec2& previous, bool focus) {
+		this->mouseMovedCB(event, previous, focus);
+});
+#else
+	PanInput* panInput = Input::get<PanInput>();
+	panInput->setFingerSensitive(true);
+	panInput->setTouchScreen(true);
+	panInput->addEndListener(LISTENER_KEY,[=](const PanEvent& event, bool focus) {
+		this->panEndedCB(event,focus);
+	});
+	panInput->addMotionListener(LISTENER_KEY,[=](const PanEvent& event, bool focus) {
+		this->panMovedCB(event,focus);
+	});
+#endif
+	_active = success;
+	return success;
+}
+
+
+/**
+* Processes the currently cached inputs.
+*
+* This method is used to to poll the current input state.  This will poll the
+* keyboad and accelerometer.
+*
+* This method also gathers the delta difference in the touches. Depending on
+* the OS, we may see multiple updates of the same touch in a single animation
+* frame, so we need to accumulate all of the data together.
+*/
+void PolyInput::update(float dt) {
+#ifndef CU_TOUCH_SCREEN
+	// DESKTOP CONTROLS
+	Keyboard* keys = Input::get<Keyboard>();
+
+	// Map "keyboard" events to the current frame boundary
+	_keyReset  = keys->keyPressed(RESET_KEY);
+	_keyExit   = keys->keyPressed(EXIT_KEY);
+
+	if (_mousepan) {
+		_pandelta = _currentTouch - _previousTouch;
+		_pandelta.y *= -1.0f;
+	}
+	else {
+		_pandelta.x = 0.0f;
+		_pandelta.y = 0.0f;
+	}
+#endif
+	_previousTouch = _currentTouch;
+
+	_resetPressed = _keyReset;
+	_exitPressed  = _keyExit;
+
+	// If it does not support keyboard, we must reset "virtual" keyboard
+#ifdef CU_TOUCH_SCREEN
+	_keyReset = false;
+#endif
+}
+
+/**
+* Clears any buffered inputs so that we may start fresh.
+*/
+void PolyInput::clear() {
+	_resetPressed = false;
+	_exitPressed  = false;
+
+	_currentTouch = Vec2::ZERO;
+	_previousTouch = Vec2::ZERO;
+  _pandelta = Vec2::ZERO;
+
+	_mousepan = false;
+}
+
+#pragma mark -
+#pragma mark Touch Callbacks
+/**
+* Callback for the end of a touch event
+*
+* @param event	   The associated event
+* @param focus     Whether the listener currently has focus
+*/
+void PolyInput::panEndedCB(const cugl::PanEvent& event, bool focus) {
+  _pandelta = Vec2::ZERO;
+}
+
+/**
+* Callback for a pan movement event
+*
+* @param event	   The associated event
+* @param focus     Whether the listener currently has focus
+*/
+void PolyInput::panMovedCB(const cugl::PanEvent& event, bool focus) {
+  if (event.fingers == NUM_PAN_FINGERS) {
+    _pandelta = event.delta;
+    _pandelta.y *= -1.0f;
+  }
+}
+
+#pragma mark -
+#pragma mark Mouse Callbacks
+
+/**
+* Called when a mouse button is initially pressed
+*
+* @param  event    The event storing the mouse state
+* @param  clicks   The number of recent clicks, including this one
+* @parm   focus	   Whether the listener currently has focus
+*/
+void PolyInput::mouseDownCB(const cugl::MouseEvent& event, Uint8 clicks, bool focus) {
+	_currentTouch = event.position;
+	_previousTouch = event.position;
+	_mousepan = true;
+}
+
+/**
+* Called when a mouse button is released
+*
+* @param  event    The event storing the mouse state
+* @param  clicks   The number of recent clicks, including this one
+* @parm   focus	   Whether the listener currently has focus
+*/
+void PolyInput::mouseUpCB(const cugl::MouseEvent& event, Uint8 clicks, bool focus) {
+	_currentTouch = event.position;
+	_previousTouch = event.position;
+	_mousepan = false;
+}
+
+/**
+* Called when the mouse is moved while held down
+*
+* @param  event    The event storing the mouse state
+* @param  previous The previous position of the mouse
+* @parm   focus	   Whether the listener currently has focus
+*/
+void PolyInput::mouseMovedCB(const cugl::MouseEvent& event, const Vec2& previous, bool focus) {
+	if (_mousepan) {
+		_currentTouch = event.position;
+	}
+}
