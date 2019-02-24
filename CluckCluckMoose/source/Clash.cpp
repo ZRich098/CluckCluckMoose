6222dd2afebb0c172a67faf55c585a8e32e8f425
dm678 <dm678@cornell.edu>
2019-02-24 18:45:07 -0500

    Second draft The Clash logic, why does this work I don't know

--- CluckCluckMoose/source/Clash.cpp
+++ CluckCluckMoose/source/Clash.cpp
@@ -3,7 +3,9 @@
 using namespace cugl;
 
 void Clash::resolve(Moose player1, Moose player2) {
-	while (!player1.getStack().empty() && !player2.getStack.empty())
+	bool empty1 = player1.getStack().empty();
+	bool empty2 = player2.getStack().empty();
+	while (!empty1 && !empty2)
 	{
 		int result = player1.getStack().front().compare(player2.getStack().front());
 		if (result == -1)
@@ -19,5 +21,8 @@ void Clash::resolve(Moose player1, Moose player2) {
 			player1.removeBottomFromStackToDiscard();
 			player2.removeBottomFromStackToDiscard();
 		}
+
+		empty1 = player1.getStack().empty();
+		empty2 = player2.getStack().empty();
 	}
 }
