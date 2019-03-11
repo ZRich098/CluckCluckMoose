104fc8ddf9fad32284fd0268be9047837500d51d
dm678 <dm678@cornell.edu>
2019-03-11 04:12:36 -0400

    moves Chicken.h vars back to private

--- CluckCluckMoose/source/Chicken.h
+++ CluckCluckMoose/source/Chicken.h
@@ -77,11 +77,10 @@ special intToSpecial(int i);
 
 class Chicken{
 	private:
-		
-	public:
 		element e;
 		special s;
 		int damage;
+	public:
 		//Constructors
 		/** Creates a standard chicken card of special s */
 		Chicken(special s);
