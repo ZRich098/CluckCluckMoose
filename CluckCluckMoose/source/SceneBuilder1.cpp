742d2b007e9f3489d938ec24cf531221b8d33239
daa98 <daa98@cornell.edu>
2019-03-16 18:45:09 -0400

    drawing scales fixed

--- CluckCluckMoose/source/SceneBuilder1.cpp
+++ CluckCluckMoose/source/SceneBuilder1.cpp
@@ -85,43 +85,43 @@ bool SceneBuilder1::init(const std::shared_ptr<cugl::AssetManager>& assets, cons
 	//Create a node for drawing chickens at each level of stacking
 	chickenCanvas1 = Node::alloc();
 	layer->addChild(chickenCanvas1);
-	chickenCanvas1->setPosition(100, 250);
+	chickenCanvas1->setPosition(50, 400);
 
 	chickenCanvas2 = Node::alloc();
 	layer->addChild(chickenCanvas2);
-	chickenCanvas2->setPosition(100, 400);
+	chickenCanvas2->setPosition(50, 510);
 
 	chickenCanvas3 = Node::alloc();
 	layer->addChild(chickenCanvas3);
-	chickenCanvas3->setPosition(100, 550);
+	chickenCanvas3->setPosition(50, 620);
 
 	chickenCanvas4 = Node::alloc();
 	layer->addChild(chickenCanvas4);
-	chickenCanvas4->setPosition(100, 250);
+	chickenCanvas4->setPosition(150, 400);
 
 	chickenCanvas5 = Node::alloc();
 	layer->addChild(chickenCanvas5);
-	chickenCanvas5->setPosition(100, 400);
+	chickenCanvas5->setPosition(150, 510);
 
 	chickenCanvas6 = Node::alloc();
 	layer->addChild(chickenCanvas6);
-	chickenCanvas6->setPosition(100, 550);
+	chickenCanvas6->setPosition(150, 620);
 
 	chickenCanvas7 = Node::alloc();
 	layer->addChild(chickenCanvas7);
-	chickenCanvas7->setPosition(100, 700);
+	chickenCanvas7->setPosition(50, 730);
 
 	chickenCanvas8 = Node::alloc();
 	layer->addChild(chickenCanvas8);
-	chickenCanvas8->setPosition(100, 850);
+	chickenCanvas8->setPosition(50, 840);
 
 	chickenCanvas9 = Node::alloc();
 	layer->addChild(chickenCanvas9);
-	chickenCanvas9->setPosition(100, 700);
+	chickenCanvas9->setPosition(150, 730);
 
 	chickenCanvas10 = Node::alloc();
 	layer->addChild(chickenCanvas10);
-	chickenCanvas10->setPosition(100, 850);
+	chickenCanvas10->setPosition(150, 840);
 
 	//Add button canvas
 	buttonCanvas = Node::alloc();
@@ -150,7 +150,7 @@ void SceneBuilder1::buildChicken(std::shared_ptr<Texture> texture, std::shared_p
 		node->removeAllChildren();
 	}
 	std::shared_ptr<PolygonNode> chick = PolygonNode::allocWithTexture(texture);
-	chick->setScale(0.3f); // Magic number to rescale asset
+	chick->setScale(0.8f); // Magic number to rescale asset
 	chick->setAnchor(Vec2::ANCHOR_CENTER);
 	chick->setPosition(posX, posY);
 	chick->flipHorizontal(flip);
@@ -178,7 +178,7 @@ void SceneBuilder1::buildGameScene() {
 	//Draw background
 	std::shared_ptr<Texture> texturebg = _assets->get<Texture>("farmbg");
 	std::shared_ptr<PolygonNode> background = PolygonNode::allocWithTexture(texturebg);
-	background->setScale(1.0f); // Magic number to rescale asset
+	background->setScale(1.2f); // Magic number to rescale asset
 	background->setAnchor(Vec2::ANCHOR_CENTER);
 	background->setPosition(SCENE_WIDTH/2, 512);
 	backCanvas->addChild(background);
@@ -186,17 +186,17 @@ void SceneBuilder1::buildGameScene() {
 	//Draw player moose
 	std::shared_ptr<Texture> textureM = _assets->get<Texture>("moose");
 	std::shared_ptr<PolygonNode> moose1 = PolygonNode::allocWithTexture(textureM);
-	moose1->setScale(0.4f); // Magic number to rescale asset
+	moose1->setScale(0.3f); // Magic number to rescale asset
 	moose1->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
-	moose1->setPosition(-100, 250);
+	moose1->setPosition(-75, 550);
 	moose1->flipHorizontal(false);
 	mooseCanvas->addChild(moose1);
 
 	//Draw opponent moose
 	std::shared_ptr<PolygonNode> moose2 = PolygonNode::allocWithTexture(textureM);
-	moose2->setScale(0.4f); // Magic number to rescale asset
+	moose2->setScale(0.3f); // Magic number to rescale asset
 	moose2->setAnchor(Vec2::ANCHOR_BOTTOM_RIGHT);
-	moose2->setPosition(SCENE_WIDTH + 100, 250);
+	moose2->setPosition(SCENE_WIDTH + 75, 550);
 	moose2->flipHorizontal(true);
 	mooseCanvas->addChild(moose2);
 
@@ -205,7 +205,7 @@ void SceneBuilder1::buildGameScene() {
 	std::shared_ptr<PolygonNode> foreground = PolygonNode::allocWithTexture(texturefg);
 	foreground->setScale(1.0f); // Magic number to rescale asset
 	foreground->setAnchor(Vec2::ANCHOR_BOTTOM_CENTER);
-	foreground->setPosition(SCENE_WIDTH/2, 0);
+	foreground->setPosition(SCENE_WIDTH/2, 300);
 	frontCanvas->addChild(foreground);
 
 	// Get chicken textures.
@@ -279,14 +279,14 @@ void SceneBuilder1::buildGameScene() {
 		id->flipHorizontal(true);
 		std::shared_ptr<Button> butt = Button::alloc(id);
 		butt->setAnchor(Vec2::ANCHOR_CENTER);
-		butt->setScale(0.2, 0.2);
+		butt->setScale(0.5, 0.5);
 
 		butt->setAnchor(Vec2::ANCHOR_CENTER);
 		if (i < 3) {
-			butt->setPosition(i * 200 - 200, 50);
+			butt->setPosition(i * 200 - 200, 150);
 		}
 		else {
-			butt->setPosition((i-3) * 200 - 200, -100);
+			butt->setPosition((i-3) * 200 - 200, 0);
 		}
 		if (_input.isActive()) {
 			//CULog("active");
@@ -356,19 +356,19 @@ void SceneBuilder1::buildGameScene() {
 			}
 		}
 		if (i == 0) {
-			buildChicken(text, chickenCanvas1, 100, 525, true);
+			buildChicken(text, chickenCanvas1, 100, 575, true);
 		}
 		else if (i == 1) {
-			buildChicken(text, chickenCanvas2, 100, 525, true);
+			buildChicken(text, chickenCanvas2, 100, 575, true);
 		}
 		else if (i == 2){
-			buildChicken(text, chickenCanvas3, 100, 525, true);
+			buildChicken(text, chickenCanvas3, 100, 575, true);
 		}
 		else if (i == 3) {
-			buildChicken(text, chickenCanvas7, 100, 525, true);
+			buildChicken(text, chickenCanvas7, 100, 575, true);
 		}
 		else {
-			buildChicken(text, chickenCanvas8, 100, 525, true);
+			buildChicken(text, chickenCanvas8, 100, 575, true);
 		}
 	}
 
@@ -425,20 +425,20 @@ void SceneBuilder1::buildGameScene() {
 			}
 		}
 		if (i == 0) {
-			buildChicken(text, chickenCanvas4, 750, 525, false);
+			buildChicken(text, chickenCanvas4, 750, 575, false);
 		}
 		else if (i == 1) {
-			buildChicken(text, chickenCanvas5, 750, 525, false);
+			buildChicken(text, chickenCanvas5, 750, 575, false);
 		}
 		else if (i == 2) {
-			buildChicken(text, chickenCanvas6, 750, 525, false);
+			buildChicken(text, chickenCanvas6, 750, 575, false);
 		}
 		else if (i == 3) {
-			buildChicken(text, chickenCanvas9, 750, 525, false);
+			buildChicken(text, chickenCanvas9, 750, 575, false);
 
 		}
 		else {
-			buildChicken(text, chickenCanvas10, 750, 525, false);
+			buildChicken(text, chickenCanvas10, 750, 575, false);
 
 		}
 	}
